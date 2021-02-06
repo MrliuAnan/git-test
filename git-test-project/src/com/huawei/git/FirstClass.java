2efec92c42694323d72be33ded8c9b60d55beb94
liuan <526004095@qq.com>
2021-02-06 17:59:35 +0800

    把本地工程添加git

---
+++ git-test-project/src/com/huawei/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.huawei.git;
+
+/**
+ * @author liuan
+ * @Description
+ * @date
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
